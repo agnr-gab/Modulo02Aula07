9a09a02339bd7ef6cbf1cb97a7899b1ddd669a5a
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-31 20:53:59 -0300

    resposta exercicio 04

--- src/br/com/zup/Lista07Ex04.java
+++ src/br/com/zup/Lista07Ex04.java
@@ -1,8 +1,22 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Lista07Ex04 {
     public static void main(String[] args) {
         // 4. Faça um programa que leia n números inteiros positivos e calcule a soma desses números.
+        Scanner leitorNumeros = new Scanner (System.in);
+        System.out.println("Digite a quantidade de numeros desejada: ");
 
+        // Variaveis
+        int soma = 0;
+        int numeros = leitorNumeros.nextInt();
+
+        for (int i = 0; i < numeros; i = i + 1) {
+            System.out.println("Digite o numero " + (i+1) +"°:");
+            int numero = leitorNumeros.nextInt();
+            soma += numero;
+        }
+        System.out.println("A soma dos números digitados é: " + soma);
     }
 }
