ca57b7a8d90334767d5ed7c03b39d0cf3cc67f7c
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-31 16:07:48 -0300

    iniciando o exercicio 02 da lista 07

---
+++ src/br/com/zup/Lista07Ex02.java
@@ -0,0 +1,7 @@
+package br.com.zup;
+
+public class Lista07Ex02 {
+    public static void main(String[] args) {
+        //2. Faça um programa que leia números inteiros e imprima sua média.
+    }
+}
