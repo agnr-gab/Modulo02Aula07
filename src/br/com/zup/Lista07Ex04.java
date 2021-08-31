25f9bea8f2630fc00397e3e23dde32730670e520
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-31 20:41:14 -0300

    exercicio 04

---
+++ src/br/com/zup/Lista07Ex04.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+public class Lista07Ex04 {
+    public static void main(String[] args) {
+        // 4. Faça um programa que leia n números inteiros positivos e calcule a soma desses números.
+
+    }
+}
