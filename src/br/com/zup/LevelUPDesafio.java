c264778036fd037a9493b981e1ec667cefbe07ed
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-31 20:25:48 -0300

    informações para resolução do level up

--- src/br/com/zup/LevelUPDesafio.java
+++ src/br/com/zup/LevelUPDesafio.java
@@ -4,5 +4,17 @@ public class LevelUPDesafio {
     public static void main(String[] args) {
         //LEVEL UP. Faça um programa que some os números ímpares contidos em um intervalo definido pelo usuário. O usuário define o valor inicial do intervalo e o valor final deste intervalo e o programa deve somar todos os números ímpares contidos neste intervalo. Caso o usuário digite um intervalo inválido (começando por um valor maior que o valor final) deve ser escrito uma mensagem de erro na tela, “Intervalo de valores inválido” e o programa termina.
 
+       /* 1- pedir para o usuario definir o intervalo.
+        -começo e fim dos numeros
+        -se o usuario digitar o intervalo invalido (verificar se o valor final < valor inicial) encerrar o programa “Intervalo de valores inválido” <- ficar dentro do else
+        criar o for dentro do if
+
+        2- o que é um numero impar (verificar se o numero é impar) //% 2 != 0
+
+        3- se o numero for impar somar a var soma */
+
+
+
+
     }
 }
