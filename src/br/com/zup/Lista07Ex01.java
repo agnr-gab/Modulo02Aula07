66b9c6717348bacc3cc6e7a34cf3503087aca632
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-31 15:33:00 -0300

    Lista de exercicios Arrays List

---
+++ src/br/com/zup/Lista07Ex01.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Lista07Ex01 {
+    public static void main(String[] args) {
+        //1 - Escreva um programa que escreva na tela, de 1 até 100, de 1 em 1, 2 vezes. A primeira vez com "for" e a segunda com "while".
+
+        git
+    }
+}
